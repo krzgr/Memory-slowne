3ba6a6748b3638fb57ba5c7e429e7b8c6d1c77bc
krzgr <kgrund2205@gmail.com>
2022-04-21 22:09:32 +0200

    Dodano kilka funkcji do wypelnienia w klasie WordMemoryGame

--- Memory-slowne/Game/WordMemoryGame.py
+++ Memory-slowne/Game/WordMemoryGame.py
@@ -1,4 +1,22 @@
+import json
 
 class WordMemoryGame:
     def __init__(self):
         pass
+
+    def setGameType(self):
+        return None
+    
+    def setDifficulty(self):
+        return None
+    
+    def startNewGame(self):
+        return None
+    
+    def getAllAnswers(self):
+        return None
+
+    def getCorrectAnswers(self):
+        return None
+    
+    
