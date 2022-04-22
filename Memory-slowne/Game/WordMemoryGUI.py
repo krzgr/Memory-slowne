86634e68c1ad869baf55079cf9730e8e7b4b0536
krzgr <kgrund2205@gmail.com>
2022-04-22 22:44:51 +0200

    Utworzono podstawowy wygląd interfejsu

--- Memory-slowne/Game/WordMemoryGUI.py
+++ Memory-slowne/Game/WordMemoryGUI.py
@@ -5,11 +5,14 @@ class WordMemoryGUI(PyQt5.QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setGeometry(0, 0, 720, 480)
+        self.setGeometry(0, 0, 1080, 720)
         self.move(self.screen().geometry().center() - self.frameGeometry().center())
         self.setWindowTitle("Memory słowne")
+
+        #self.setFixedSize(1080, 720)
         
         self._addMenuBar()
+        self._renderGame()
     
     def _addMenuBar(self):
         menubar = self.menuBar()
@@ -26,8 +29,68 @@ class WordMemoryGUI(PyQt5.QtWidgets.QMainWindow):
         self.statsMenu.addAction(self.allStatsAction)
         menubar.addMenu(self.statsMenu)
 
-        self.helpMenu = PyQt5.QtWidgets.QMenu("&Pomoc", self)
-        menubar.addMenu(self.helpMenu)
+        menubar.addAction(self.helpAction)
+
+
+    def _renderGame(self):
+        self.mainWidget = PyQt5.QtWidgets.QWidget()
+        self.mainWidget.setContentsMargins(15, 15, 15, 15)
+        self.setCentralWidget(self.mainWidget)
+
+        
+        self.mainGridLayout = PyQt5.QtWidgets.QGridLayout(self.centralWidget())
+
+        spacerItem1 = PyQt5.QtWidgets.QSpacerItem(40, 20, PyQt5.QtWidgets.QSizePolicy.Expanding, PyQt5.QtWidgets.QSizePolicy.Minimum)
+        self.mainGridLayout.addItem(spacerItem1, 1, 2, 1, 1)
+
+        spacerItem2 = PyQt5.QtWidgets.QSpacerItem(40, 20, PyQt5.QtWidgets.QSizePolicy.Expanding, PyQt5.QtWidgets.QSizePolicy.Minimum)
+        self.mainGridLayout.addItem(spacerItem2, 1, 0, 1, 1)
+
+        spacerItem3 = PyQt5.QtWidgets.QSpacerItem(20, 40, PyQt5.QtWidgets.QSizePolicy.Minimum, PyQt5.QtWidgets.QSizePolicy.Expanding)
+        self.mainGridLayout.addItem(spacerItem3, 2, 1, 1, 1)
+
+        spacerItem4 = PyQt5.QtWidgets.QSpacerItem(20, 40, PyQt5.QtWidgets.QSizePolicy.Minimum, PyQt5.QtWidgets.QSizePolicy.Expanding)
+        self.mainGridLayout.addItem(spacerItem4, 0, 1, 1, 1)
+
+        self.gameGridLayout = PyQt5.QtWidgets.QGridLayout()
+        self.gameGridLayout.setSizeConstraint(PyQt5.QtWidgets.QLayout.SetMinimumSize)
+        self.gameGridLayout.setSpacing(25)
+        self.mainGridLayout.addLayout(self.gameGridLayout, 1, 1, 1, 1)        
+        
+        self.buttonList = [PyQt5.QtWidgets.QPushButton("Bratysława", self.centralWidget()) for i in range(0, 40)]
+
+        for i in range(0, len(self.buttonList)):
+            self.buttonList[i].setStyleSheet("QPushButton { font-size: 20px; }")
+            self.gameGridLayout.addWidget(self.buttonList[i], i // 5, i % 5)
+
+        
+        self.buttonList[5].setText("Konstantyno")
+
+        self.timeVerticalLayout = PyQt5.QtWidgets.QVBoxLayout()
+
+        spacerItem5 = PyQt5.QtWidgets.QSpacerItem(20, 200, PyQt5.QtWidgets.QSizePolicy.Expanding, PyQt5.QtWidgets.QSizePolicy.Maximum)
+        self.timeVerticalLayout.addItem(spacerItem5)
+
+        self.timerLabel1 = PyQt5.QtWidgets.QLabel("Czas:", self.centralWidget())
+        self.timerLabel2 = PyQt5.QtWidgets.QLabel("1:00", self.centralWidget())
+
+        font = PyQt5.QtGui.QFont()
+        font.setPointSize(24)
+        self.timerLabel1.setFont(font)
+        self.timerLabel1.setAlignment(PyQt5.QtCore.Qt.AlignCenter)
+        self.timerLabel2.setFont(font)
+        self.timerLabel2.setAlignment(PyQt5.QtCore.Qt.AlignCenter)
+
+        self.timerLabel1.setMargin(10)
+        self.timerLabel2.setMargin(5)
+        
+        self.timeVerticalLayout.addWidget(self.timerLabel1)
+        self.timeVerticalLayout.addWidget(self.timerLabel2)
+
+        spacerItem6 = PyQt5.QtWidgets.QSpacerItem(20, 200, PyQt5.QtWidgets.QSizePolicy.Expanding, PyQt5.QtWidgets.QSizePolicy.Maximum)
+        self.timeVerticalLayout.addItem(spacerItem6)
+
+        self.mainGridLayout.addLayout(self.timeVerticalLayout, 1, 3)
 
 
     def _createMenuBarActions(self):
@@ -37,3 +100,10 @@ class WordMemoryGUI(PyQt5.QtWidgets.QMainWindow):
 
         self.myStatsAction = PyQt5.QtWidgets.QAction("&Moje", self)
         self.allStatsAction = PyQt5.QtWidgets.QAction("&Wszystkich", self)
+
+        self.helpAction = PyQt5.QtWidgets.QAction("&Pomoc", self)
+        self.helpAction.triggered.connect(self._onHelpClicked)
+    
+
+    def _onHelpClicked(self):
+        print("Clicked!")
