d551be4b4eb3d1d91a1c0b75c94045cea3d84ea0
krzgr <kgrund2205@gmail.com>
2022-04-22 14:03:19 +0200

    Dodano pasek menu

--- Memory-slowne/Game/WordMemoryGUI.py
+++ Memory-slowne/Game/WordMemoryGUI.py
@@ -5,6 +5,35 @@ class WordMemoryGUI(PyQt5.QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setGeometry(400, 400, 720, 480)
+        self.setGeometry(0, 0, 720, 480)
+        self.move(self.screen().geometry().center() - self.frameGeometry().center())
         self.setWindowTitle("Memory słowne")
+        
+        self._addMenuBar()
     
+    def _addMenuBar(self):
+        menubar = self.menuBar()
+        self._createMenuBarActions()
+        
+        self.gameMenu = PyQt5.QtWidgets.QMenu("&Gra", self)
+        self.gameMenu.addAction(self.settingsAction)
+        self.gameMenu.addSeparator()
+        self.gameMenu.addAction(self.exitAction)
+        menubar.addMenu(self.gameMenu)
+
+        self.statsMenu = PyQt5.QtWidgets.QMenu("&Statystyki", self)
+        self.statsMenu.addAction(self.myStatsAction)
+        self.statsMenu.addAction(self.allStatsAction)
+        menubar.addMenu(self.statsMenu)
+
+        self.helpMenu = PyQt5.QtWidgets.QMenu("&Pomoc", self)
+        menubar.addMenu(self.helpMenu)
+
+
+    def _createMenuBarActions(self):
+        self.settingsAction = PyQt5.QtWidgets.QAction("&Ustawienia", self)
+        self.exitAction = PyQt5.QtWidgets.QAction("&Wyjście", self)
+        self.exitAction.triggered.connect(self.close)
+
+        self.myStatsAction = PyQt5.QtWidgets.QAction("&Moje", self)
+        self.allStatsAction = PyQt5.QtWidgets.QAction("&Wszystkich", self)
