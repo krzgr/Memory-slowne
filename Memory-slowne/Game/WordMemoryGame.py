3b31fd42a28034a53c567dee4bb77def01969189
Jakub Kolton <koltonjakub45@gmail.com>
2022-04-30 13:57:44 +0200

    dodano funkcje z issues 4, nie dokonczono funckji startNewGame()

--- Memory-slowne/Game/WordMemoryGame.py
+++ Memory-slowne/Game/WordMemoryGame.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 import json
 import random
 
 class WordMemoryGame:
+
     def __init__(self):
         self.difficulty = "easy"
         self.wordsList = []
@@ -11,11 +14,24 @@ class WordMemoryGame:
         self.wordsDict = {}
         self.allAnswers = []
         self.correctAnswers = []
+        self.playerSelectedWords = []
+
+    def startNewGame(self):
+        self.shuffleWords()
+
+    def playerWin(self):
+        for elem in self.playerSelectedWords:
+            if elem not in self.correctAnswers:
+                return False
+        return True
+
+    def isGameFinished(self):
+        pass
 
     # funkcje typu load
     def loadWordsFromFile(self):
         try:
-            wordsFile = open("data/words.json", "r")
+            wordsFile = open("data/words.json", "r", encoding="utf-8")
             self.wordsDict = json.loads(wordsFile.read())  # wszystkie słowa wpisane z pliku
         except FileNotFoundError:
             return False
@@ -26,7 +42,7 @@ class WordMemoryGame:
 
     def loadPlayersDataFromFile(self):
         try:
-            playersFile = open("data/players.json", "r")
+            playersFile = open("data/players.json", "r", encoding="utf-8")
             self.playersDict = json.loads(playersFile.read())
         except FileNotFoundError:
             return False
@@ -44,8 +60,14 @@ class WordMemoryGame:
         try:
             self.allAnswers = random.sample(self.wordsList, self.numberOfAllAnswers) #klikalne odpowiedzi
             self.correctAnswers = random.sample(self.allAnswers, self.numberOfCorrectAnswers) # poprawne odpowiedzi wylosowane
+
+            if (len(self.playerSelectedWords) != 0):
+                (self.playersDict[self.nickname])[1] += 1
+
         except Exception:
             return False
+
+        self.playerSelectedWords = []
         return True
 
 
@@ -61,6 +83,16 @@ class WordMemoryGame:
 
     def getAllPlayersStatistics(self):
         return [(elem, self.playersDict[elem]) for elem in self.playersDict]
+
+    def getNumberOfCorrectAnswers(self):
+        return self.numberOfCorrectAnswers
+
+    def getNumberOfAllAnswers(self):
+        return self.numberOfAllAnswers
+
+    # wzór: 5 * 2^(self.numberOfCorrectAnswers // 5)
+    def getDurationOfLookingAtCorrectAnswersAsSeconds(self):
+        return 5 * pow(2, self.numberOfCorrectAnswers // 5)
 
     # funckje z gracz
     def setNickname(self, nicknameInput):
@@ -79,7 +111,7 @@ class WordMemoryGame:
 
     def savePlayerStatistics(self):
         try:
-            playersFile = open("data/players.json", "w")
+            playersFile = open("data/players.json", "w", encoding="utf-8")
             jsonDict = json.dumps(self.playersDict)
         except FileNotFoundError:
             return False
@@ -89,3 +121,7 @@ class WordMemoryGame:
         else:
             playersFile.write(jsonDict)
         return True
+
+    def addPlayerAnswer(self, word):
+        self.playerSelectedWords.append(word)
+        #return True
