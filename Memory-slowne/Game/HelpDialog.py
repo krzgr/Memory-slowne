3fe272cb97832f6c830cd5f2456a88985fc78d67
krzgr <kgrund2205@gmail.com>
2022-05-30 18:26:05 +0200

    Wyjustowano tekst w oknie pomocy

--- Memory-slowne/Game/HelpDialog.py
+++ Memory-slowne/Game/HelpDialog.py
@@ -17,7 +17,8 @@ class HelpDialog(PyQt5.QtWidgets.QDialog):
         self.helpLabel = PyQt5.QtWidgets.QLabel("Witaj w grze Memory słowne!\nTwoim celem będzie zapamiętanie określonej przez Ciebie ilości słów, a następnie wskazanie ich poprzez kliknęcie na ekranie. Nasz program oferuje kilka poziomów trudności, a także tryb gry na czas. Wszystkie te ustawienia możesz wybrać w oknie ustawień, znajdziesz je w panelu menu w górze otwartego okna. Następnie wybierz opcję nowa gra. Na twoim ekranie pojawią się słowa, które masz za zadanie zapamiętać. Gdy będziesz gotowy, kliknij start, a przed tobą pojawią się słowa spośród których masz wybrać poprawne. Jeśli wybrałeś opcję gry na czas, rozpoczęcie gry nastąpi automatycznie po upływie czasu na zapamiętanie słów.\nPowodzenia!")
         self.helpLabel.setWordWrap(True)
         self.mainVerticalLayout.addWidget(self.helpLabel)
-        self.helpLabel.setStyleSheet("QLabel { font-size: 14px; }")
+        self.helpLabel.setStyleSheet("QLabel { font-size: 14px; padding: 12px;}")
+        self.helpLabel.setAlignment(PyQt5.QtCore.Qt.AlignmentFlag.AlignJustify)
         
         self.buttonBox = PyQt5.QtWidgets.QDialogButtonBox()
         self.buttonBox.setStandardButtons(PyQt5.QtWidgets.QDialogButtonBox.Ok)
